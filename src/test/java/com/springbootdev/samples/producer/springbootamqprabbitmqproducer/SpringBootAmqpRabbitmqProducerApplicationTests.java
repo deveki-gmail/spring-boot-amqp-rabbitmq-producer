2b0063c0e6bae9bab6e5611dcbef013d5712162f
deveki-gmail <devekinandan@gmail.com>
2019-01-13 18:38:58 +0530

    code added

---
+++ src/test/java/com/springbootdev/samples/producer/springbootamqprabbitmqproducer/SpringBootAmqpRabbitmqProducerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springbootdev.samples.producer.springbootamqprabbitmqproducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAmqpRabbitmqProducerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
